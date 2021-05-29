e6e3a05c356c61d0aa9850ac3180a1c813ef9ca6
dev_ans <arthurnx98@gmail.com>
2021-05-29 13:54:58 -0300

    Iniciada criação da tela home do sistema

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,39 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
+				<div class="panel-heading">
+					<h1>
+						Olá,
+						<span>{{ Auth::user()->name }}</span>
+						!
+					</h1>
+					<p>O que deseja acessar?</p>
+					<div class="row">
+						<div class="col-sm-4">
+							<a class="btn btn-primary btn-lg" style="width: 100%; height: 200px" href="#" role="button">
+								<div style="margin-top: 50px;">NewsLetters</div>
+								<div>
+									<h1 class="glyphicon glyphicon-envelope"></h1>
+								</div>
+							</a>
+						</div>
+						<div class="col-sm-4">
+							<a class="btn btn-primary btn-lg" style="width: 100%; height: 200px" href="#" role="button">
+								<div style="margin-top: 50px;">Pessoas</div>
+								<div>
+									<h1 class="glyphicon glyphicon-user"></h1>
+								</div>
+							</a>
+						</div>
+						<div class="col-sm-4">
+							<a class="btn btn-primary btn-lg" style="width: 100%; height: 200px" href="#" role="button">
+								<div style="margin-top: 50px;">Relatório</div>
+								<div>
+									<h1 class="glyphicon glyphicon-file"></h1>
+								</div>
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
