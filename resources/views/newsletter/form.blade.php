7d5a938c276d383a0349f486ae9efdb8ab571044
dev_ans <arthurnx98@gmail.com>
2021-05-30 19:15:33 -0300

    Adicionado editor de texto para criação/edição da newsletter

--- resources/views/newsletter/form.blade.php
+++ resources/views/newsletter/form.blade.php
@@ -16,10 +16,14 @@
                     <form>
                         <div class="form-group">
                             <label for="titulo">Título</label>
-                            <input type="titulo" class="form-control" id="titulo" placeholder="Título">
+                            <input type="titulo" style="width: 30%;" class="form-control" id="titulo" name="titulo" placeholder="Título">
                         </div>
 
-                        <div class="">
+                        <div class="form-group">
+                            <textarea id="text" name="text" placeholder="Escreva seu NewsLetter aqui!"></textarea>
+                        </div>
+
+                        <div class="form-group">
                             <button type="submit" class="btn btn-success">Confirmar</button>
                             <a type="button" class="btn btn-danger" href="{{ url('home/newsletter') }}">Cancelar</a>
                         </div>
@@ -31,4 +35,25 @@
 		</div>
 	</div>
 </div>
-@endsection('content')
+
+<!-- Adição do editor de texto Tiny e seus plugins -->
+<script src="https://cdn.tiny.cloud/1/d6myxrpdwwlhmpojcuo1s0kpjj5ng0is5p7ilo1qds07czq4/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea',
+        height: 400,
+        menubar: false,
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+        ],
+        toolbar: 'undo redo | fontselect fontsizeselect formatselect | ' +
+        'bold italic forecolor backcolor code | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent | ' +
+        'removeformat | fullscreen | help',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+    });
+</script>
+  
+  @endsection('content')
